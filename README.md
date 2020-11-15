20c10a6590d397ff7862b9040097b9a448350df6
levymetal <noreply@levymetal.net>
2020-11-15 14:09:41 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+![js](https://github.com/levymetal/connect-four/workflows/javascript/badge.svg?branch=main)
+
+### What
+
+A connect-four win detector. Not the dryest solution but reasonably efficient and easy to follow.
+
+### Requirements
+
+Node 14 or later.
+
+### Setup
+
+`npm i`
+
+### Testing
+
+`npm test`
---
+++ detector.js
@@ -0,0 +1,31 @@
+class Detector {
+  constructor(seed) {
+    if (typeof seed !== 'string' || seed.length !== 7 * 6 || seed.match(/[^012]/)) throw new Error('invalid seed');
+    this.board = seed.match(/.{7}/g).map(row => row.split('').map(Number));
+  }
+
+  get winner() {
+    for (let y = 0; y < 6; y++) {
+      for (let x = 0; x < 7; x++) {
+        const winner =
+          new Vector((_, i) => this.board[y + 0]?.[x + i]).winner || // horizontal
+          new Vector((_, i) => this.board[y + i]?.[x + 0]).winner || // vertical
+          new Vector((_, i) => this.board[y + i]?.[x - i]).winner || // positive diagonal
+          new Vector((_, i) => this.board[y + i]?.[x + i]).winner; // negative diagonal
+        if (winner) return winner;
+      }
+    }
+  }
+}
+
+class Vector {
+  constructor(builder) {
+    this.vector = [...Array(4)].map(builder);
+  }
+
+  get winner() {
+    return [1, 2].find(player => this.vector.every(cell => cell === player));
+  }
+}
+
+module.exports = Detector;
